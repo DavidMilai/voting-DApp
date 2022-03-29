8595565e76162e4775cb2b9f2ad5a85b3b541514
DavidMilai <davidmilai67@gmail.com>
2022-03-29 09:17:25 +0300

    constants

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+String kInfuraUrl =
+    "https://rinkeby.infura.io/v3/e9c90ade8f1a421a8d2522d925383ff5";
+
+String kPrivateKey =
+    "entry lonely vault cute select scatter artwork glow bone return fuel method";
