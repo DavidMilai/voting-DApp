dc3f38f098ac40c0a4c79434cf98ad4c68710996
DavidMilai <davidmilai67@gmail.com>
2022-03-29 09:17:43 +0300

    web 3 client setup

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:voting_app/constants.dart';
+import 'package:web3dart/web3dart.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +11,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Client? httpClient;
+  Web3Client? web3Client;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    httpClient = Client();
+    web3Client = Web3Client(kInfuraUrl, httpClient!);
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
