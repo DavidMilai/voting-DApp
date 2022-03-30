072233f4642e4af535c29e052b6e83486afe5678
DavidMilai <davidmilai67@gmail.com>
2022-03-30 08:58:24 +0300

    loading function

---
+++ lib/services/functions_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/services.dart';
+import 'package:voting_app/constants.dart';
+import 'package:web3dart/web3dart.dart';
+
+Future<DeployedContract> loadContract() async {
+  ByteData abi = await rootBundle.load('assets/abi.json');
+
+  String contractAddress = kContractAddress;
+
+  final contract = DeployedContract(
+      ContractAbi.fromJson(abi as String, "election"),
+      EthereumAddress.fromHex(contractAddress));
+
+  return contract;
+}
